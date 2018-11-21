6acef7f3d697c52d87948f37efde2555a797623b
Zarif <zarif_41@mail.ru>
2018-11-21 11:56:39 +0300

    some changes

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 window.onload = function () {
-  const elem = document.getElementById('element');
+  var elem = document.getElementById('element');
   elem.classList.remove('hide_page');
   elem.classList.add('show_page');
 };
-const navMain = document.querySelector('.nav__container');
-const navToggle = document.querySelector('.mobile-button');
-const navToggleCl = document.querySelector('.case--close-nav');
+var navMain = document.querySelector('.nav__container');
+var navToggle = document.querySelector('.mobile-button');
+var navToggleCl = document.querySelector('.case--close-nav');
 navMain.classList.remove('nav--nojs');
 navToggle.addEventListener('click', function () {
   if (navMain.classList.contains('nav--closed')) {
@@ -23,23 +23,23 @@ navToggleCl.addEventListener('click', function () {
 go();
 window.addEventListener('resize', go);
 function go(){
-  let currentWidth = window.innerWidth;
+  const currentWidth = window.innerWidth;
   if (currentWidth <= 680) {
     const baseWidth = 680;
     const baseHeightSecon = 1360;
     const baseHeightFirst = 1183;
-    let newWidth = baseWidth - currentWidth;
-    let step = newWidth * 2;
-    let finalHeightSecond = baseHeightSecon - step;
-    let finalHeightFirst = baseHeightFirst - step;
+    var newWidth = baseWidth - currentWidth;
+    var step = newWidth * 2;
+    var finalHeightSecond = baseHeightSecon - step;
+    var finalHeightFirst = baseHeightFirst - step;
   } else if (currentWidth >= 681 && currentWidth <= 1920) {
     const baseWidth = 1920;
     const baseHeightSecon = 920;
     const baseHeightFirst = 877;
-    let newWidth = baseWidth - currentWidth;
-    let step = newWidth / 2;
-    let finalHeightSecond = baseHeightSecon - step;
-    let finalHeightFirst = baseHeightFirst - step;
+    var newWidth = baseWidth - currentWidth;
+    var step = newWidth / 2;
+    var finalHeightSecond = baseHeightSecon - step;
+    var finalHeightFirst = baseHeightFirst - step;
   }
   document.querySelector('.first-screen').style.height = finalHeightFirst + 'px';
   document.querySelector('.gallery').style.height = finalHeightSecond + 'px';
